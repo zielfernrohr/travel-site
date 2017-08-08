67a6c28a19bb08d84887fe00ed10fbde77299412
Martin <martin.welter@hotmail.com>
2017-08-08 14:17:46 +0200

    Bearbeitungsstand 2017-08-07

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,10 @@
 var gulp = require('gulp'),
-watch = require('gulp-watch');
+watch = require('gulp-watch'),
+postcss = require('gulp-postcss'),
+autoprefixer = require('autoprefixer'),
+cssvars = require('postcss-simple-vars'),
+nested = require('postcss-nested')
+cssImport = require('postcss-import');
 
 gulp.task('default', function() {
   console.log("Hooray - you created a Gulp task.");
@@ -10,7 +15,9 @@ gulp.task('html', function() {
 });
 
 gulp.task('styles', function() {
-  console.log("Imagine Sass or PostCSS tasks running here.");
+  return gulp.src('./app/assets/styles/styles.css')
+  .pipe(postcss([cssImport, cssvars, nested, autoprefixer]))
+  .pipe(gulp.dest('./app/temp/styles'));
 });
 
 gulp.task('watch', function() {
