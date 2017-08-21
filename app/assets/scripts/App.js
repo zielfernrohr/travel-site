c3df6af8c284ca0ce209a792efca9a61428f1c4f
Martin <martin.welter@hotmail.com>
2017-08-21 17:53:56 +0200

    Finished mobile header

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,19 +1,3 @@
-var $ = require('jquery');
-import Person from './modules/Person';
+import MobileMenu from './modules/MobileMenu';
 
-class Adult extends Person {
-  payTaxes() {
-    console.log(this.name + " now owes §0 in taxes.");
-  }
-}
-
-// alert("ABC 321");
-
-var john = new Person("John Doe", "orange");
-john.greet();
-
-var jane = new Adult("Jane Smith", "green");
-jane.greet();
-jane.payTaxes();
-
-// $("h1").remove();
+var mobileMenu = new MobileMenu();
